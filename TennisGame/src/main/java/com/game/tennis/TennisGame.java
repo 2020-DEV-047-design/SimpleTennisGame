6f2bd16651211c419915023dc0fdd2e6c13f7300
Yubraj Subedi <yubraj.subedi@capgemini.com>
2020-02-25 23:01:30 +0530

    Updating business code for winning situation after an advantage point

--- TennisGame/src/main/java/com/game/tennis/TennisGame.java
+++ TennisGame/src/main/java/com/game/tennis/TennisGame.java
@@ -57,7 +57,9 @@ public class TennisGame {
 	 */
 	public String getScore() {
 		if (player1.getPoints() >= 3 && player2.getPoints() >= 3) {
-            if (player1.getPoints() == player2.getPoints()) {
+            if (Math.abs(player2.getPoints() - player1.getPoints()) >= 2) {
+            	return getLeadingScorer().getName() + TennisGameConstants.WON;
+            } else if (player1.getPoints() == player2.getPoints()) {
             	return TennisGameConstants.PLAYERS_ARE + TennisGameConstants.DEUCE;
             } else {
             	return getLeadingScorer().getName() + TennisGameConstants.HAS + TennisGameConstants.ADVANTAGE;
