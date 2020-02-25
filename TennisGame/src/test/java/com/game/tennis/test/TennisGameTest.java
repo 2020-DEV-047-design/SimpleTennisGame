f78378c5bc59a3ef60c036cd9c18d93a234695db
Yubraj Subedi <yubraj.subedi@capgemini.com>
2020-02-25 16:28:23 +0530

    Committing test case for addScorePoint() which adds player's score

--- TennisGame/src/test/java/com/game/tennis/test/TennisGameTest.java
+++ TennisGame/src/test/java/com/game/tennis/test/TennisGameTest.java
@@ -7,6 +7,7 @@ import org.junit.Test;
 
 import com.game.tennis.TennisGame;
 import com.game.tennis.exception.InvalidScoreException;
+import com.game.tennis.pojo.Player;
 import com.game.tennis.util.TennisGameConstants;
 
 /**
@@ -17,31 +18,47 @@ import com.game.tennis.util.TennisGameConstants;
  */
 
 public class TennisGameTest {
-	
-	TennisGame tennisGame;
-	
+
+	TennisGame tennisGame, tennisMatch;
+	Player player1, player2;
+
 	@Before
-	public void Setup(){
-		tennisGame = new TennisGame();
+	public void Setup() {
+		tennisGame = new TennisGame(null, null);
+		player1 = new Player();
+		player2 = new Player();
+		tennisMatch = new TennisGame(player1, player2);
 	}
+
 	/**
 	 * Test case to get the score description that will be scored by a player
 	 * points{0,1,2,3} These values correspond to {LOVE,FIFTEEN,THIRTY,FORTY}
 	 */
 	@Test
-	public void testScoreDescForLove_Fifteen_Thirty_Forty(){		 
+	public void testScoreDescForLove_Fifteen_Thirty_Forty() {
 		assertEquals(TennisGameConstants.SCORE_LOVE, tennisGame.getScoreDesc(0));
 		assertEquals(TennisGameConstants.SCORE_FIFTEEN, tennisGame.getScoreDesc(1));
 		assertEquals(TennisGameConstants.SCORE_THIRTY, tennisGame.getScoreDesc(2));
 		assertEquals(TennisGameConstants.SCORE_FORTY, tennisGame.getScoreDesc(3));
 	}
-	
+
 	/**
-	 * Test case to check for an exception in case any invalid point is supplied as score
-	 * Any value apart from {0,1,2,3} are the invalid points
+	 * Test case to check for an exception in case any invalid point is supplied
+	 * as score Any value apart from {0,1,2,3} are the invalid points
 	 */
 	@Test(expected = InvalidScoreException.class)
-	public void testScoreDescForValueOtherThan_0_1_2_3_ThrowsException(){
+	public void testScoreDescForValueOtherThan_0_1_2_3_ThrowsException() {
 		assertEquals(new InvalidScoreException(TennisGameConstants.INVALID_SCORE_POINT), tennisGame.getScoreDesc(4));
 	}
+
+	/**
+	 * Test case to add points scored by a player to his/her account
+	 */
+	@Test
+	public void testaddScorePointForPointsOfPlayers() {
+		tennisGame.addScorePoint(player1); // when point is added one time into player1's account 
+		assertEquals(1, player1.getPoints());
+		assertEquals(0, player2.getPoints());// when no point has been added to player2's account
+	}
+
 }
