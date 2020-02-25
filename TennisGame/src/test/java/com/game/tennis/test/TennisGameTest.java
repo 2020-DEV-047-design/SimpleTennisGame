8b09b0f98fc5cc0cf3da0e193e118ec673394ae5
Yubraj Subedi <yubraj.subedi@capgemini.com>
2020-02-25 13:18:40 +0530

    Committing test case for testing score Fifteen scenario

--- TennisGame/src/test/java/com/game/tennis/test/TennisGameTest.java
+++ TennisGame/src/test/java/com/game/tennis/test/TennisGameTest.java
@@ -2,6 +2,7 @@ package com.game.tennis.test;
 
 import static org.junit.Assert.assertEquals;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import com.game.tennis.TennisGame;
@@ -15,13 +16,20 @@ import com.game.tennis.util.TennisGameConstants;
  */
 
 public class TennisGameTest {
+	
+	TennisGame tennisGame;
+	
+	@Before
+	public void Setup(){
+		tennisGame = new TennisGame();
+	}
 	/**
 	 * Test case to get the score description that will be scored by a player
 	 * points{0,1,2,3} These values correspond to {LOVE,FIFTEEN,THIRTY,FORTY}
 	 */
 	@Test
-	public void testScoreDescForLove() {
-		TennisGame tennisGame = new TennisGame();
+	public void testScoreDescForLove_Fifteen() {		 
 		assertEquals(TennisGameConstants.SCORE_LOVE, tennisGame.getScoreDesc(0));
+		assertEquals(TennisGameConstants.SCORE_FIFTEEN, tennisGame.getScoreDesc(1));
 	}
 }
