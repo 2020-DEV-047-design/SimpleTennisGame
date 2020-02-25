137aba02e37adf7a8e97dde6babfee1ea23149ea
Yubraj Subedi <yubraj.subedi@capgemini.com>
2020-02-25 14:28:06 +0530

    Committing test case for testing invalid score scenario

--- TennisGame/src/test/java/com/game/tennis/test/TennisGameTest.java
+++ TennisGame/src/test/java/com/game/tennis/test/TennisGameTest.java
@@ -6,6 +6,7 @@ import org.junit.Before;
 import org.junit.Test;
 
 import com.game.tennis.TennisGame;
+import com.game.tennis.exception.InvalidScoreException;
 import com.game.tennis.util.TennisGameConstants;
 
 /**
@@ -28,10 +29,19 @@ public class TennisGameTest {
 	 * points{0,1,2,3} These values correspond to {LOVE,FIFTEEN,THIRTY,FORTY}
 	 */
 	@Test
-	public void testScoreDescForLove_Fifteen_Thirty() {		 
+	public void testScoreDescForLove_Fifteen_Thirty_Forty(){		 
 		assertEquals(TennisGameConstants.SCORE_LOVE, tennisGame.getScoreDesc(0));
 		assertEquals(TennisGameConstants.SCORE_FIFTEEN, tennisGame.getScoreDesc(1));
 		assertEquals(TennisGameConstants.SCORE_THIRTY, tennisGame.getScoreDesc(2));
 		assertEquals(TennisGameConstants.SCORE_FORTY, tennisGame.getScoreDesc(3));
+	}
+	
+	/**
+	 * Test case to check for an exception in case any invalid point is supplied as score
+	 * Any value apart from {0,1,2,3} are the invalid points
+	 */
+	@Test(expected = InvalidScoreException.class)
+	public void testScoreDescForValueOtherThan_0_1_2_3_ThrowsException(){
+		assertEquals(new InvalidScoreException(TennisGameConstants.INVALID_SCORE_POINT), tennisGame.getScoreDesc(4));
 	}
 }
