5e604bf83de9c7894e483506e1be0a0a9a781f6d
Yubraj Subedi <yubraj.subedi@capgemini.com>
2020-02-25 12:58:19 +0530

    Committing constants file for score "ZERO"

---
+++ TennisGame/src/main/java/com/game/tennis/util/TennisGameConstants.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+package com.game.tennis.util;
+
+/**
+ * This interface contains the constants used in the Tennis Game application
+ * @author 2020-DEV-047
+ *
+ */
+public interface TennisGameConstants {
+	
+	String SCORE_LOVE = "LOVE";
+	
+}
